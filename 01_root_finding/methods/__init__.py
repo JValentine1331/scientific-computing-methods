231ca6db1541c1f189c211cea39bf013c91e3b19
Jess Riley Valentine <jesswaldroup282@gmail.com>
2026-06-19 10:42:45 -0700

    Backup before repository restructuring

---
+++ 01_root_finding/methods/__init__.py
@@ -0,0 +1,7 @@
+from .bisection import bisection
+from .false_position import false_position
+from .fixed_point import fixed_point
+from .modified_newton import modified_newton
+from .newton import newton
+from .secant import secant
+from .steffensen import steffensen
